b2fe088b3bc6526f694fb6b5119f22c6d6478dce
mc975 <mc975@mc975deMacBook-Pro.local>
2018-03-28 00:06:18 +0800

    2018/3/28 00:06

--- src/pages/about-nesting-tabs/about-nesting-tabs.ts
+++ src/pages/about-nesting-tabs/about-nesting-tabs.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { HomeListValPage } from '../home-list-val/home-list-val';
 /**
  * Generated class for the AboutNestingTabsPage page.
  *
@@ -14,8 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'about-nesting-tabs.html',
 })
 export class AboutNestingTabsPage {
-
+  tab1 = HomeListValPage;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+   
   }
 
   ionViewDidLoad() {
